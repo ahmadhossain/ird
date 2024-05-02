1c037422b42f4a5aa5e0036da7cd7e4079f5e9bd
Ahmad Hossain <ahmadhossain.bu@gmail.com>
2024-05-02 20:14:33 +0600

    icon issue fix

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -8,31 +8,35 @@ import { BiSolidDonateHeart } from "react-icons/bi";
 
 const Sidebar = () => {
   return (
-    <IconContext.Provider
-      value={{
-        className: "p-3 hover:bg-gray-100 fill-gray-500 rounded-md",
-      }}
-    >
-      {/* <div className="relative"> */}
-      <div className="min-[1024px]:flex hidden flex-col justify-center items-center">
-        <div className="flex items-center flex-col gap-y-6 mt-12 p-4">
-          <GoHome size={50} />
-          <TbBooks
-            color="white"
-            className="bg-[#2b9e76] hover:bg-[#2b9e76]"
-            size={50}
-          />
-          <BsBook size={50} />
-          <CiBookmark size={50} />
-          <BsGrid size={50} />
-          <BsSend size={50} />
-        </div>
+    <div className="min-[1024px]:flex hidden flex-col justify-center items-center">
+      <div className="flex items-center flex-col gap-y-6 mt-12 p-4">
+        <GoHome
+          className="p-3 hover:bg-gray-100 fill-gray-500 rounded-md"
+          size={50}
+        />
+        <TbBooks
+          color="white"
+          className="bg-[#2b9e76] hover:bg-[#2b9e76] p-3 fill-gray-500 rounded-md"
+          size={50}
+        />
+        <BsBook
+          className="p-3 hover:bg-gray-100 fill-gray-500 rounded-md"
+          size={50}
+        />
+        <CiBookmark
+          className="p-3 hover:bg-gray-100 fill-gray-500 rounded-md"
+          size={50}
+        />
+        <BsGrid
+          className="p-3 hover:bg-gray-100 fill-gray-500 rounded-md"
+          size={50}
+        />
+        <BsSend
+          className="p-3 hover:bg-gray-100 fill-gray-500 rounded-md"
+          size={50}
+        />
       </div>
-      {/* </div> */}
-      {/* <div className="fixed px-4 py-3 bg-[#2b9e76] rounded-lg">
-        <BiSolidDonateHeart color="white" size={23} />
-      </div> */}
-    </IconContext.Provider>
+    </div>
   );
 };
 
